6f23334adef614d98f017e99506468432f6589e3
Mateus <143225198+mtssantana@users.noreply.github.com>
2024-04-04 16:51:21 -0300

    Add files via upload

---
+++ a.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site</title>
+    
+    <h2>Página do usuário</h2>
+</head>
+<body>
+    <p>
+    <label for="name">Nome</label><br>
+    <input required type="text" id="name" name="nome" size="35">
+    </p>
+    <p>
+    <label for="data">Data de Nascimento</label><br>
+    <input type="date" id="data" name="data" required>
+    </p>   
+    <p>
+        <label for="sobrenome">Sobrenome</label><br>
+        <input type="text" name="sobrenome" id="sobrenome" required>
+    </p>
+    <p>
+    <label for="Endereço">Endreço</label><br>
+    <input type="text" name="endreço" id="endreço" required>
+    </p>
+    <input type="submit" value="Enviar" name="Enviar" placeholder="enviar">
+
+
+ 
+
+</body>
+</html>
