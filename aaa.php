50497b3816d1d963c0290cc9c89128834963561f
Mateus <143225198+mtssantana@users.noreply.github.com>
2024-04-04 17:35:23 -0300

    Add files via upload

--- aaa.php
+++ aaa.php
@@ -4,42 +4,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Site</title>
-    
-    <h2>Página do usuário</h2>
 </head>
 <body>
+<h2>Página do usuário</h2>
 <form method="post" action="">
     <p>
-    <label for="name">Nome</label><br>
-    <input required type="text" id="name" name="nome">
+        <label for="name">Nome</label><br>
+        <input required type="text" id="name" name="name">
     </p>
     <p>
         <label for="sobrenome">Sobrenome</label><br>
         <input type="text" name="sobrenome" id="sobrenome" required>
     </p>
     <p>
-    <label for="data">Data de Nascimento</label><br>
-    <input type="date" id="data" name="data" required>
-    </p>   
-  
-    <p>
-    <label for="Endereço">Endreço</label><br>
-    <input type="text" name="endreço" id="endreço" required>
+        <label for="data">Data de Nascimento</label><br>
+        <input type="date" id="data" name="data" required>
     </p>
-    <input type="submit" value="Enviar" name="Enviar" placeholder="enviar">
-
-<?php
-
-
-if (empty($_POST["name"]) ||(empty($_POST["sobrenome"])) ) {
-    echo "Digite seu nome e sobrenome";
-}  else { 
-    echo "Bem-Vindo";
-
-
-}
-?>
- 
-
+    <p>
+        <label for="endereco">Endereço</label><br>
+        <input type="text" name="endereco" id="endereco" required>
+    </p>
+    <input type="submit" value="Enviar">
+</form>
+<div id="mensagemBoasVindas">
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (!empty($_POST['name']) && !empty($_POST['sobrenome']) && !empty($_POST['data'])) {
+            $name = $_POST['name'];
+            $sobrenome = $_POST['sobrenome'];
+            echo "Bem-vindo, $name!";
+        }
+    }
+    ?>
+</div>
 </body>
 </html>
